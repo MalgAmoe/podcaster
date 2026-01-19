3d0ccbf19b65f8ca6120446ec42345baf9bebae9
MalgAmoe <malgamoe@protonmail.com>
2026-01-19 17:23:52 +0100

    add page

--- backend/lib/poddyclip_backend_web/controllers/page_controller.ex
+++ backend/lib/poddyclip_backend_web/controllers/page_controller.ex
@@ -1,6 +1,32 @@
 defmodule PoddyclipBackendWeb.PageController do
   use PoddyclipBackendWeb, :controller
 
+  # Public pages
+  def landing(conn, _params) do
+    conn
+    |> put_layout(false)
+    |> render(:landing)
+  end
+
+  def pricing(conn, _params) do
+    conn
+    |> put_layout(false)
+    |> render(:pricing)
+  end
+
+  def terms(conn, _params) do
+    conn
+    |> put_layout(false)
+    |> render(:terms)
+  end
+
+  def privacy(conn, _params) do
+    conn
+    |> put_layout(false)
+    |> render(:privacy)
+  end
+
+  # Authenticated app
   def process(conn, _params) do
     # Use only root layout (no app layout) - root already has the navbar
     conn
--- backend/lib/poddyclip_backend_web/user_auth.ex
+++ backend/lib/poddyclip_backend_web/user_auth.ex
@@ -194,7 +194,7 @@ defmodule PoddyclipBackendWeb.UserAuth do
     end
   end
 
-  defp signed_in_path(_conn), do: ~p"/"
+  defp signed_in_path(_conn), do: ~p"/app"
 
   @doc """
   Plug for routes that require the user to be authenticated.
