1258d4160b895e628257f3b2a7f8bbd8cd745ba6
MalgAmoe <malgamoe@protonmail.com>
2026-01-29 20:27:53 +0100

    add webhook url

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -196,6 +196,12 @@ if config_env() == :prod do
 
   config :poddyclip_backend, PoddyclipBackendWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: [
+      "https://#{host}",
+      "https://www.#{host}",
+      "https://munchycow.com",
+      "https://www.munchycow.com"
+    ],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
